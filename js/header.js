09f0a023de38d7ff057df81c36d4cd67c6526f47
tainamiranda2 <tainaferreira0987@gmail.com>
2024-05-27 19:31:03 -0300

    iniciando projeto

---
+++ js/header.js
@@ -0,0 +1,18 @@
+document.getElementById('header').innerHTML = `
+  <header>
+  <img src="../img/chocolateH.jpg" />
+  <img src="../img/logo.jpg" />
+    <h1>Saboreando - Bolo de Pote</h1>
+
+    <input placeholder="Buscar produtos por nome ou descrição"/>
+    <button>Meus pedidos</button>
+    <button>Taxa e tempo de entrega</button>
+    <nav>
+      <ul>
+        <li><a href="#home">Bolo de pote tradicional</a></li>
+        <li><a href="#about">Bolo de pote especial</a></li>
+        <li><a href="#contact">Bebidas</a></li>
+      </ul>
+    </nav>
+  </header>
+`;
